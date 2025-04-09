dc1daa4b1ee378b13f19adaf962ea610c4ee090a
Zeynep Oksuzler <zeyokss5@gmail.com>
2025-04-09 16:01:34 +0300

    add route for adding flashcards

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:sqflite_common_ffi/sqflite_ffi.dart';
-import 'allScreens/home_screen.dart';
-import 'allScreens/flashcard_form.dart';
+import 'package:flipera/allScreens/home_screen.dart';
+import 'package:flipera/allScreens/flashcard_form.dart'; // <-- FlashcardForm burada
 
 void main() {
-  // Initialize FFI for non-mobile platforms
-  sqfliteFfiInit();
-  databaseFactory = databaseFactoryFfi;
-  
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  runApp(
+    MaterialApp(
       title: 'FlipEra',
       theme: ThemeData(
         fontFamily: 'Montserrat',
         primarySwatch: Colors.cyan,
       ),
       debugShowCheckedModeBanner: false,
-      initialRoute: '/',
+      home: const HomeScreen(),
       routes: {
-        '/': (context) => const HomeScreen(),
-        '/add': (context) => const FlashcardForm(),
+        '/add': (context) => const FlashcardForm(), // <-- Burası eklendi
       },
-    );
-  }
+    ),
+  );
 }
